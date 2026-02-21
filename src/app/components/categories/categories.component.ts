3dbee50e935a1f1ae2d153362a8e401714d690aa
unnath <timeandwind01@gmail.com>
2026-02-21 10:02:36 +0530

    Setup the initial project stucture and empty files

---
+++ src/app/components/categories/categories.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories.component',
+  imports: [],
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.css',
+})
+export class CategoriesComponent {
+
+}
---
+++ src/app/components/questions/question/question.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question.component',
+  imports: [],
+  templateUrl: './question.component.html',
+  styleUrl: './question.component.css',
+})
+export class QuestionComponent {
+
+}
